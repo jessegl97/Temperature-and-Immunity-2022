a0e0e94859ffc4ea314f50a3ee7518f2471752d5
a-arneson <aga98@vt.edu>
2024-02-26 14:39:47 -0500

    Finished up variability models!

--- meanModels/jesseVarModels.R
+++ meanModels/jesseVarModels.R
@@ -177,26 +177,28 @@ ggplot(ti.pi, aes(x = temp, y = elisa_od))+
 # New plan! Get error for each birds obs as it differs from group mean.
 
 # Calculate means - need to add separate ones for each dpi if interested in that
-meancc <- mean(coldcon$elisa_od, na.rm =T)
-meanci <- mean(coldinf$elisa_od, na.rm =T)
-meanwc <- mean(warmcon$elisa_od, na.rm =T)
-meanwi <- mean(warminf$elisa_od, na.rm =T)
 
-coldcon$res <- abs(coldcon$elisa_od - meancc)
-coldinf$res <- abs(coldinf$elisa_od - meanci)
-warmcon$res <- abs(warmcon$elisa_od - meanwc)
-warminf$res <- abs(warminf$elisa_od - meanwi)
-
-dat <- rbind(coldcon, coldinf, warmcon, warminf)
+medelisa  <- median(ti.pi$elisa_od, na.rm = T)
+meanelisa  <- mean(ti.pi$elisa_od, na.rm = T)
 
 
-# Model
+ggplot(data = ti.pi, aes(x = dpi, y = elisa_od))+
+  geom_jitter(width = 0.1, aes(color = temp))+
+  geom_hline(yintercept = medelisa)+
+  geom_hline(yintercept = meanelisa, color = "green")+
+  facet_wrap(~treatment)
 
-mod <- glm(res ~ temp*treatment*dpi, data = dat,
-           family = Gamma())
+ti.pi$res <- abs(ti.pi$elisa_od - medelisa)
 
-hist(residuals(mod))
-plot(residuals(mod), predict(mod))
+
+# Model - there is no random effect because it didn't help the model at all
+
+mod <- glmmTMB(res ~ temp*treatment*dpi,
+               data = ti.pi,
+               family = Gamma())
+summary(mod)
+
+simulateResiduals(mod, plot = T)
 
 car::Anova(mod, type = "III")
 
@@ -209,17 +211,15 @@ car::Anova(modstep, type = "III")
 emmeans(modstep, pairwise ~ treatment, adjust = "tukey",
         type = "response")
 
-ggplot(dat, aes(x = dpi, y = res, color = temp))+
-  geom_jitter(width = 0.1)+
-  facet_wrap(~treatment)
+emmeans(modstep, pairwise ~ temp:dpi, adjust = "tukey",
+        type = "response")
 
+#These results will take some nuance to understand
 
-ggplot(dat, aes(x = elisa_od, y = res))+
-  geom_point()
+#### Eye Score - I think I need a categorical measure of variability
 
-
-
-#### Eye Score
+#Variability in categorical variables can be calculated as 
+# 1-sum((proportions in eachgroup)^2)
 
 # Antibody Assay
 
@@ -233,26 +233,91 @@ ti.mg$band_number <- as.factor(ti.mg$band_number)
 
 #### Within-Bird Variability
 
+# Make a little table that shows counts for each bird in each category
+
+wibird <- ti.mg %>% group_by(band_number, treatment, temp, total_eye_score) %>%
+  summarize(n = n())
+
+wibird <- wibird %>% na.omit(total_eye_score)
+
+wibird <- wibird %>% group_by(band_number) %>% mutate(prop = n/sum(n)) %>%
+  ungroup()
+
+# Make even tinier table with the variability measure
+#    - closer to 1 = more variable :)
+
+wibird$propsq <- wibird$prop^2
+
+wibirdVar <- wibird %>% group_by(band_number, treatment, temp) %>%
+  summarize(catVar = 1-sum(propsq))
+
+# Model variability
+
+hist(wibirdVar$catVar) #weird bimodal distribution. Gonna use quasibinomial
+
+# No random effect needed because only one PV per bird.
+
+mod <- glm(catVar ~ temp, data = wibirdVar, family = quasibinomial())
+
+summary(mod)
+
+car::Anova(mod, type = "III")
+
+#### Between-Bird Variability
+
+# New plan! Get error for each birds obs as it differs from group mean.
+
+# Add difference column
+meanes <- mean(ti.mg$total_eye_score, na.rm = T)
+medianes <- median(ti.mg$total_eye_score, na.rm = T)
+
+ti.mg$res <- abs(ti.mg$total_eye_score-meanes)
+
+# Model
+
+hist(ti.mg$res)
+
+range(ti.mg$res, na.rm =T)
+
+mod <- glmmTMB(res ~ temp+dpi + (1|band_number),
+               data = ti.mg, 
+               family = Gamma())
+
+# Tried AR1 covariance structure and it was too complicated to calculate
+# Also tried zi model and nothing was significant and fit was no better
+
+summary(mod)
+
+simulateResiduals(mod, plot = T) #Pretty weird residuals
+# I think this is because the outcome is actually categorical.
+
+car::Anova(mod, type = "III")
+
+cld(emmeans(mod, pairwise ~ dpi, adjust = "tukey", type = "response"))
+
+# All in all, this analysis is weird and really since this is a poisson variable
+#   the mean tells us about the variance too. I think this is giving us sensible
+#   results, so I think it's fine to use as long as we lean on the conservative
+#   side for stating results.
+
+###### Fever
+source("dataCleaning_fever.R")
+
+#### Within-Bird Variability
+
 # Make the different day eye score values their own columns
 
 # Simplify data set so pivot_wider works
-ti.mg <- ti.mg %>% select(dpi, band_number, treatment, temp,
-                          sex, total_eye_score)
-
-ti.mg <- ti.mg %>% na.omit(total_eye_score)
+ti.f <- ti.f %>% select(dpi, band_number, treatment, temp,
+                          sex, fever_score)
 
 unique(ti.mg$dpi)
 
 # Add tiny constant to values so I can use PVs
-ti.mg$total_eye_score <- ti.mg$total_eye_score +0.01
 
-wibird <- ti.mg %>% pivot_wider(names_from = dpi,
+wibird <- ti.f %>% pivot_wider(names_from = dpi,
                                 names_glue = "{.value}_{dpi}",
-                                values_from = total_eye_score)
-
-
-
-
+                                values_from = fever_score)
 
 # Calcuate a PV for each bird
 
@@ -262,11 +327,10 @@ pvs <- c()
 
 for (i in 1:nrow(wibird)){
   
-  vec <- c(wibird$`total_eye_score_-12`[i], wibird$total_eye_score_3[i],
-           wibird$`total_eye_score_7`[i], wibird$total_eye_score_9[i],
-           wibird$`total_eye_score_14`[i], wibird$total_eye_score_18[i],
-           wibird$`total_eye_score_21`[i], wibird$total_eye_score_24[i],
-           wibird$`total_eye_score_28`[i], wibird$total_eye_score_35[i])
+  vec <- c(wibird$`fever_score_0`[i], wibird$fever_score_3[i],
+           wibird$`fever_score_7`[i], wibird$fever_score_14[i],
+           wibird$`fever_score_18`[i], wibird$fever_score_24[i],
+           wibird$`fever_score_28`[i], wibird$fever_score_35[i])
   
   pvs[i] <- PV(vec)
   
@@ -274,20 +338,20 @@ for (i in 1:nrow(wibird)){
 
 wibird$pv <- pvs
 
-#Graph to see how they differ
+#Graph to see how they look
 
 ggplot(wibird, aes(x = treatment, y = pv))+
   geom_jitter()+
   facet_wrap(~temp)
 
+
 # Model PVs
 
-hist(wibird$pv) #weird bimodal distribution... ZI necessary
+hist(wibird$pv)
 
 # No random effect needed because only one PV per bird.
 
-mod <- glmmTMB(pv ~ temp, data = wibird, family = ziGamma(),
-               ziformula = ~temp)
+mod <- glmmTMB(pv ~ temp+treatment, data = wibird, family = Gamma())
 
 summary(mod)
 
@@ -299,94 +363,38 @@ car::Anova(mod, type = "III")
 
 # New plan! Get error for each birds obs as it differs from group mean.
 
-# Get rid of my tiny constant from before
-ti.mg$total_eye_score <- ti.mg$total_eye_score - 0.01
+# Add difference column
+meanfev <- mean(ti.f$fever_score, na.rm = T)
 
-# Make separate data sets
-coldneg12 <- ti.mg %>% filter(temp == "Cold"& dpi == "-12")
-cold3 <- ti.mg %>% filter(temp == "Cold"& dpi == "3")
-cold7 <- ti.mg %>% filter(temp == "Cold"& dpi == "7")
-cold9 <- ti.mg %>% filter(temp == "Cold"& dpi == "9")
-cold14 <- ti.mg %>% filter(temp == "Cold"& dpi == "14")
-cold18 <- ti.mg %>% filter(temp == "Cold"& dpi == "18")
-cold21 <- ti.mg %>% filter(temp == "Cold"& dpi == "21")
-cold24 <- ti.mg %>% filter(temp == "Cold"& dpi == "24")
-cold28 <- ti.mg %>% filter(temp == "Cold"& dpi == "28")
-cold35 <- ti.mg %>% filter(temp == "Cold"& dpi == "35")
-
-warmneg12 <- ti.mg %>% filter(temp == "warm"& dpi == "-12")
-warm3 <- ti.mg %>% filter(temp == "warm"& dpi == "3")
-warm7 <- ti.mg %>% filter(temp == "warm"& dpi == "7")
-warm9 <- ti.mg %>% filter(temp == "warm"& dpi == "9")
-warm14 <- ti.mg %>% filter(temp == "warm"& dpi == "14")
-warm18 <- ti.mg %>% filter(temp == "warm"& dpi == "18")
-warm21 <- ti.mg %>% filter(temp == "warm"& dpi == "21")
-warm24 <- ti.mg %>% filter(temp == "warm"& dpi == "24")
-warm28 <- ti.mg %>% filter(temp == "warm"& dpi == "28")
-warm35 <- ti.mg %>% filter(temp == "warm"& dpi == "35")
-
-# Calculate means
-meancneg12 <- mean(coldneg12$total_eye_score, na.rm =T)
-meanc3 <- mean(cold3$total_eye_score, na.rm =T)
-meanc7 <- mean(cold7$total_eye_score, na.rm =T)
-meanc9 <- mean(cold9$total_eye_score, na.rm =T)
-meanc14 <- mean(cold14$total_eye_score, na.rm =T)
-meanc18 <- mean(cold18$total_eye_score, na.rm =T)
-meanc21 <- mean(cold21$total_eye_score, na.rm =T)
-meanc24 <- mean(cold2412$total_eye_score, na.rm =T)
-meanc28 <- mean(cold28$total_eye_score, na.rm =T)
-meanc35 <- mean(cold35$total_eye_score, na.rm =T)
-
-meanwneg12 <- mean(warmneg12$total_eye_score, na.rm =T)
-meanw3 <- mean(warm3$total_eye_score, na.rm =T)
-meanw7 <- mean(warm7$total_eye_score, na.rm =T)
-meanw9 <- mean(warm9$total_eye_score, na.rm =T)
-meanw14 <- mean(warm14$total_eye_score, na.rm =T)
-meanw18 <- mean(warm18$total_eye_score, na.rm =T)
-meanw21 <- mean(warm21$total_eye_score, na.rm =T)
-meanw24 <- mean(warm2412$total_eye_score, na.rm =T)
-meanw28 <- mean(warm28$total_eye_score, na.rm =T)
-meanw35 <- mean(warm35$total_eye_score, na.rm =T)
-
-coldneg12$res <- abs(coldneg12$total_eye_score - meanc)
-cold3$res <- abs(cold3$total_eye_score - meanc)
-cold7$res <- abs(cold7$total_eye_score - meanc)
-cold9$res <- abs(cold9$total_eye_score - meanc)
-cold14$res <- abs(cold14$total_eye_score - meanc)
-cold18$res <- abs(cold18$total_eye_score - meanc)
-cold21$res <- abs(cold21$total_eye_score - meanc)
-cold24$res <- abs(cold24$total_eye_score - meanc)
-cold28$res <- abs(cold28$total_eye_score - meanc)
-cold35$res <- abs(cold35$total_eye_score - meanc)
-
-warmneg12$res <- abs(warmneg12$total_eye_score - meanc)
-warm3$res <- abs(warm3$total_eye_score - meanc)
-warm7$res <- abs(warm7$total_eye_score - meanc)
-warm9$res <- abs(warm9$total_eye_score - meanc)
-warm14$res <- abs(warm14$total_eye_score - meanc)
-warm18$res <- abs(warm18$total_eye_score - meanc)
-warm21$res <- abs(warm21$total_eye_score - meanc)
-warm24$res <- abs(warm24$total_eye_score - meanc)
-warm28$res <- abs(warm28$total_eye_score - meanc)
-warm35$res <- abs(warm35$total_eye_score - meanc)
-
-
-dat <- rbind(coldneg12, cold3, cold7, cold9, cold14,
-             cold18, cold21, cold24, cold28, cold35,
-             warmneg12, warm3, warm7, warm9, warm14,
-             warm18, warm21, warm24, warm28, warm35)
-
+ti.f$res <- abs(ti.f$fever_score-meanfev)
 
 # Model
 
-hist(dat$res)
+hist(ti.f$res)
 
-mod <- glm(res ~ temp+dpi, data = dat, family = Gamma())
+ti.f$dpi <- as.factor(ti.f$dpi)
+
+mod <- glmmTMB(res ~ temp*treatment*dpi + (1|band_number),
+               data = ti.f, family = gaussian())
 
 summary(mod)
 
 hist(residuals(mod))
-plot(residuals(mod), predict(mod))
+plot(residuals(mod), predict(mod)) #Assumptions look okay
 
-car::Anova(mod, type = "III")
+car::Anova(mod, type = "III") # Gotta keep the fat model
 
+cld(emmeans(mod, pairwise ~ temp:treatment:dpi, adjust = "tukey", type = "response"))
+#Godspeed....
+# JK I'll help you sort through these lol
+
+
+
+############### Some notes ########################
+#
+# I think we definitely have the data to talk about within bird
+# variability. For between bird variability, we *technically*
+# don't have replication, so it is a little weird to use tests.
+# That being said, I don't see anything wrong with reporting these
+# and just being conservative about reporting the results.
+#
